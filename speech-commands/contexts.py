0b14293df6f13e218ca5ebd4762631c9d4b55a65
Evan <evfredericksen@gmail.com>
2021-05-23 23:47:57 -0400

    contexts

--- speech-commands/contexts.py
+++ speech-commands/contexts.py
@@ -4,7 +4,14 @@ from dragonfly import AppContext, FuncContext
 PYTHON = "python"
 JAVASCRIPT = "javascript"
 
+VISUAL_STUDIO_CODE = "VISUAL_STUDIO_CODE"
+
 language = PYTHON
+manual_app_context = None
+
+def set_manual_app_context(app_context: str):
+    global manual_app_context
+    manual_app_context = app_context
 
 
 def set_language(lang):
@@ -12,7 +19,7 @@ def set_language(lang):
     language = lang
 
 
-vscode = AppContext(title="Visual Studio Code")
+vscode = AppContext(title="Visual Studio Code") | FuncContext(lambda *a: manual_app_context == VISUAL_STUDIO_CODE)
 visual_studio = AppContext(title="Visual Studio") & ~vscode
 firefox = AppContext(title="Mozilla Firefox")
 chrome = AppContext(title="Google Chrome")
