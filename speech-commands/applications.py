4c2e4afe4493a35ea39df8bba9213d6a36e94ced
Evan <evfredericksen@gmail.com>
2022-08-15 21:41:00 -0400

    f

--- speech-commands/applications.py
+++ speech-commands/applications.py
@@ -15,7 +15,7 @@ applications = {
         "title": "google chrome",
         "executable": "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
     },
-    "terminal": {"title": "efrederick2@", "executable": "wt.exe"},
+    "terminal": {"title": "efredericks2@", "executable": "wt.exe"},
     "command prompt": {"title": ["command prompt", "cmd.exe"], "executable": "cmd.exe"},
     "powershell": {"title": "windows powershell"},
     "git bash": {
