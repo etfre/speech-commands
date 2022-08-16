0214aa5fbc4e2aa3f24645ea3ac9800d286b173b
Evan <evfredericksen@gmail.com>
2022-08-15 20:59:21 -0400

    .

--- speech-commands/vscode.py
+++ speech-commands/vscode.py
@@ -118,9 +118,9 @@ if utils.IS_MAC:
     non_repeat_mapping['fuzzy'] = "{w-p}"
     non_repeat_mapping['new tab'] = "{w-n}"
     non_repeat_mapping['(search file) | (file search)'] = "{w-f}"
-    non_repeat_mapping["(search project) | (project search)"] = "{ws-f}",
-    non_repeat_mapping["(replace [in] file) | (file replace)"] = "{wa-f}",
-    non_repeat_mapping["(replace [in] project) | (project replace)"] = "{sw-h}",
+    non_repeat_mapping["(search project) | (project search)"] = "{ws-f}"
+    non_repeat_mapping["(replace [in] file) | (file replace)"] = "{wa-f}"
+    non_repeat_mapping["(replace [in] project) | (project replace)"] = "{sw-h}"
 
 repeat_mapping = {
     "flip north": "{a-up}", 
