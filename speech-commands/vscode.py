e163588120d6b659f1581f559e32f8e2d8d29624
Evan <evfredericksen@gmail.com>
2022-08-15 21:38:17 -0400

    fsc

--- speech-commands/vscode.py
+++ speech-commands/vscode.py
@@ -125,7 +125,7 @@ if utils.IS_MAC:
     non_repeat_mapping["(replace [in] file) | (file replace)"] = "{wa-f}"
     non_repeat_mapping["(replace [in] project) | (project replace)"] = "{sw-h}"
     non_repeat_mapping["file explorer"] = "{ws-e}"
-    non_repeat_mapping["source control"] = "{ws-g}g"
+    non_repeat_mapping["source control"] = "{cs-g}"
 
 repeat_mapping = {
     "flip north": "{a-up}", 
